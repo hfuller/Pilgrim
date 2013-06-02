35d40e78a07805bcb40c86d7b614bb010a90a533
Jake <unknownhybrid@gmail.com>
2013-06-02 14:15:39 -0500

    added data attribute indexing
    also reformatted the boardjson so it's readable AT ALL

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,61 @@
 $(document).ready(function() { 
 
 //now usually I don't do this but uh
-var boardjson = '{ "width":6, "rows": [ [ {"type": "water", "rank": 12}, {"type": "water", "rank": 12}, {"type": "water", "rank": 12}, {"type": "water", "rank": 12} ], [ {"type": "water", "rank": 12}, {"type": "wood", "rank": 12}, {"type": "wood", "rank": 10}, {"type": "brick", "rank": 4}, {"type": "water", "rank": 12} ], [ {"type": "water", "rank": 12}, {"type": "wheat", "rank": 6}, {"type": "sheep", "rank": 8}, {"type": "sheep", "rank": 4}, {"type": "ore", "rank": 5}, {"type": "water", "rank": 12} ], [ {"type": "water", "rank": 12}, {"type": "wood", "rank": 11}, {"type": "brick", "rank": 2}, {"type": "brick", "rank": 8}, {"type": "wheat", "rank": 6}, {"type": "wheat", "rank": 9}, {"type": "water", "rank": 12} ], [ {"type": "water", "rank": 12}, {"type": "sheep", "rank": 11}, {"type": "wood", "rank": 5}, {"type": "sheep", "rank": 9}, {"type": "wood", "rank": 3}, {"type": "water", "rank": 12} ], [ {"type": "water", "rank": 12}, {"type": "ore", "rank": 10}, {"type": "ore", "rank": 3}, {"type": "desert", "rank": 12}, {"type": "water", "rank": 12} ], [ {"type": "water", "rank": 12}, {"type": "water", "rank": 12}, {"type": "water", "rank": 12}, {"type": "water", "rank": 12} ] ] , "rank": 12}';
+var boardjson = '{ '
+							+ '"width":6, '
+							+ '"rows": ['
+								+ '[ '
+									+ '{"type": "water", "rank": 12},'
+									+ '{"type": "water", "rank": 12},'
+									+ '{"type": "water", "rank": 12},'
+									+ '{"type": "water", "rank": 12}'
+								+ '], '
+								+ '[ '
+									+ '{"type": "water", "rank": 12},'
+									+ '{"type": "wood", "rank": 12}, '
+									+ '{"type": "wood", "rank": 10}, '
+									+ '{"type": "brick", "rank": 4}, '
+									+ '{"type": "water", "rank": 12} '
+								+ '], '
+								+ '[ '
+									+ '{"type": "water", "rank": 12}, '
+									+ '{"type": "wheat", "rank": 6}, '
+									+ '{"type": "sheep", "rank": 8}, '
+									+ '{"type": "sheep", "rank": 4}, '
+									+ '{"type": "ore", "rank": 5}, '
+									+ '{"type": "water", "rank": 12} '
+								+ '], '
+								+ '[ '
+									+ '{"type": "water", "rank": 12}, '
+									+ '{"type": "wood", "rank": 11}, '
+									+ '{"type": "brick", "rank": 2}, '
+									+ '{"type": "brick", "rank": 8}, '
+									+ '{"type": "wheat", "rank": 6}, '
+									+ '{"type": "wheat", "rank": 9}, '
+									+ '{"type": "water", "rank": 12}'
+								+ '], '
+								+ '['
+									+ '{"type": "water", "rank": 12}, '
+									+ '{"type": "sheep", "rank": 11},'
+									+ '{"type": "wood", "rank": 5},'
+									+ '{"type": "sheep", "rank": 9},'
+									+ '{"type": "wood", "rank": 3},'
+									+ '{"type": "water", "rank": 12}'
+								+ '], '
+								+ '[ '
+									+ '{"type": "water", "rank": 12}, '
+									+ '{"type": "ore", "rank": 10}, '
+									+ '{"type": "ore", "rank": 3}, '
+									+ '{"type": "desert", "rank": 12}, '
+									+ '{"type": "water", "rank": 12} '
+								+ '], '
+								+ '[ '
+									+ '{"type": "water", "rank": 12}, '
+									+ '{"type": "water", "rank": 12}, '
+									+ '{"type": "water", "rank": 12}, '
+									+ '{"type": "water", "rank": 12} '
+								+ ']'
+							+ '] }';
 //go head and break 'em off with a little preview of the remix
 var boardobj = eval('(' + boardjson + ')');
 
@@ -28,7 +82,7 @@ for ( var i = 0; i < boardobj.rows.length; i++ ) {
 					   + ((boardobj.rows[i][j].rank == "4" || boardobj.rows[i][j].rank == "10") ? "three-dot" : "")
 					   + ((boardobj.rows[i][j].rank == "3" || boardobj.rows[i][j].rank == "11") ? "two-dot" : "")
 					   + ((boardobj.rows[i][j].rank == "2" || boardobj.rows[i][j].rank == "12") ? "one-dot" : "");*/
-		eRow.append('<div class="' + hexClasses+ '"><span class="' + rankClasses + '">' + boardobj.rows[i][j].rank + '</span></div>');
+		eRow.append('<div class="' + hexClasses+ '" data-x="' + i + '" data-y="' + j + '"><span class="' + rankClasses + '">' + boardobj.rows[i][j].rank + '</span></div>');
 	}
 	
 }
