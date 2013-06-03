1cfb5800553d46af87d5277bbd9a70218240a3a3
root <root@ubuntu.(none)>
2013-06-03 16:59:34 -0500

    Add missing intersections in bottom half of grid.  Committer: mmcnellis <molloy.mcnellis@gmail.com>
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   js/functions.js

--- js/functions.js
+++ js/functions.js
@@ -35,50 +35,50 @@ $(document).ready(function() {
 	    for ( var i = 0; i < boardobj.rows.length; i++ ) {
 		
 			if ( i%2 == 0 ) {
-				var eRow = $('<div class="hex-row even"></div>');
+			    var eRow = $('<div class="hex-row even"></div>');
 			} else {
-				var eRow = $('<div class="hex-row"></div>');
+			    var eRow = $('<div class="hex-row"></div>');
 			}
-			$("#board").append(eRow);
+		$("#board").append(eRow);
+		
+		oRow = boardobj.rows[i];
+		for ( var j = 0; j < (boardobj.width-1 - oRow.length)/2; j++ ) {
+		    eRow.append('<div class="hex padding"></div>');
+		}
+		for ( var j = 0; j < oRow.length; j++ ) {
+		    
+		    //add our hex
+		    var hexHTML = '<div class="hex ' + resourceNumToString(boardobj.rows[i][j].type);
+		    hexHTML += '" data-x="' + i + '" data-y="' + j + '"></div>';
+		    //the reason I use .appendTo() is so the hex var contains the hex element that is created.
+		    //if I did eRow.append(hexHTML) then jQuery would attempt to chain it, and eRow would be stored into hex var.
+		    var hex = $(hexHTML).appendTo(eRow);
+		    
+		    //do we need to add a rank?
+		    if ( [ 'desert', 'water' ].indexOf( resourceNumToString(boardobj.rows[i][j].type) ) == -1 ) {
+			//well, it's not desert or water, so let's add one
+			hex.append('<span class="rank">' + boardobj.rows[i][j].rank + '</span>');
+		    }
+		    
+		    //just a demo to add some intersections (if we're not on the bottom row)
+		    if ( i < boardobj.rows.length - 1) {
 			
-			oRow = boardobj.rows[i];
-			for ( var j = 0; j < (boardobj.width-1 - oRow.length)/2; j++ ) {
-				eRow.append('<div class="hex padding"></div>');
+			//if we're not on the right edge of the board, we'll add a bottom-right intersection (hence the "right" class)
+			if (j < oRow.length - 1) {
+			    hex.append('<span class="intersect right ' + 
+				       (i%2 == 0 ? 'blue' : 'red') +
+				       '" data-a="' + i + '" data-b="' + j + '" data-c="1"></span>');
 			}
-			for ( var j = 0; j < oRow.length; j++ ) {
-				
-				//add our hex
-				var hexHTML = '<div class="hex ' + resourceNumToString(boardobj.rows[i][j].type);
-				hexHTML += '" data-x="' + i + '" data-y="' + j + '"></div>';
-				//the reason I use .appendTo() is so the hex var contains the hex element that is created.
-				//if I did eRow.append(hexHTML) then jQuery would attempt to chain it, and eRow would be stored into hex var.
-				var hex = $(hexHTML).appendTo(eRow);
-				
-				//do we need to add a rank?
-				if ( [ 'desert', 'water' ].indexOf( resourceNumToString(boardobj.rows[i][j].type) ) == -1 ) {
-					//well, it's not desert or water, so let's add one
-					hex.append('<span class="rank">' + boardobj.rows[i][j].rank + '</span>');
-				}
-
-				//just a demo to add some intersections (if we're not on the bottom row)
-				if ( i < boardobj.rows.length - 1) {
-					
-					//if we're not on the right edge of the board, we'll add a bottom-right intersection (hence the "right" class)
-					if (j < oRow.length - 1) {
-						hex.append('<span class="intersect right ' + 
-							(i%2 == 0 ? 'blue' : 'red') +
-							'" data-a="' + i + '" data-b="' + j + '" data-c="1"></span>');
-					}
-					
-					//and we'll add a bottom intersection for the top four rows.
-					if(i < 3) {
-						hex.append('<span class="intersect bottom ' + 
-							(i%2 == 0 ? 'blue' : 'red') +
-							'" data-a="' + i + '" data-b="' + j + '" data-c="0"></span>');
-					}		
-				}
-
-			}
+			
+			//and we'll add a bottom intersection for the .
+			if(i < 3 || j > 0 && j < oRow.length - 1) {
+			    hex.append('<span class="intersect bottom ' + 
+				       (i%2 == 0 ? 'blue' : 'red') +
+				       '" data-a="' + i + '" data-b="' + j + '" data-c="0"></span>');
+			}		
+		    }
+		    
+		}
 	    }
 	    
 	});
