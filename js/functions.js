bda81b261c1496eabe7676cfad38dc6a34d578b8
root <root@ubuntu.(none)>
2013-06-03 06:04:55 -0500

    Add spans that denote where hexagon tiles intersect  Committer: mmcnellis <molloy.mcnellis@gmail.com>

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 $(document).ready(function() { 
-
-ResourcesEnum = {
+    
+    ResourcesEnum = {
 	DESERT : 0,
 	WOOD : 1,
 	WOOL : 2,
@@ -8,55 +8,57 @@ ResourcesEnum = {
 	BRICK : 4,
 	ORE : 5,
 	WATER : 6
-}
-function resourceNumToString(resourceNum){
+    }
+    function resourceNumToString(resourceNum){
 	switch(resourceNum){
-		case ResourcesEnum.DESERT:
-			return "desert";
-		case ResourcesEnum.WOOD:
-			return "wood";
-		case ResourcesEnum.WOOL:
-			return "wool";
-		case ResourcesEnum.WHEAT:
-			return "wheat";
-		case ResourcesEnum.BRICK:
-			return "brick";
-		case ResourcesEnum.ORE:
-			return "ore";
-		case ResourcesEnum.WATER:
-		default:
-			return "water";
+	case ResourcesEnum.DESERT:
+	    return "desert";
+	case ResourcesEnum.WOOD:
+	    return "wood";
+	case ResourcesEnum.WOOL:
+	    return "wool";
+	case ResourcesEnum.WHEAT:
+	    return "wheat";
+	case ResourcesEnum.BRICK:
+	    return "brick";
+	case ResourcesEnum.ORE:
+	    return "ore";
+	case ResourcesEnum.WATER:
+	default:
+	    return "water";
 	}
-}
-//now usually I don't do this but uh
-$.getJSON('http://pixilic.com/apps/pilgrim/board_debug.php?callback=?')
-.done( function(boardobj) {
-
-	for ( var i = 0; i < boardobj.rows.length; i++ ) {
+    }
+    //now usually I don't do this but uh
+    $.getJSON('http://pixilic.com/apps/pilgrim/board_debug.php?callback=?')
+	.done( function(boardobj) {
+	    
+	    for ( var i = 0; i < boardobj.rows.length; i++ ) {
 		
 		if ( i%2 == 0 ) {
-			var eRow = $('<div class="hex-row even"></div>');
+		    var eRow = $('<div class="hex-row even"></div>');
 		} else {
-			var eRow = $('<div class="hex-row"></div>');
+		    var eRow = $('<div class="hex-row"></div>');
 		}
 		$("#board").append(eRow);
 		
 		oRow = boardobj.rows[i];
 		for ( var j = 0; j < (boardobj.width-1 - oRow.length)/2; j++ ) {
-			eRow.append('<div class="hex padding"></div>');
+		    eRow.append('<div class="hex padding"></div>');
 		}
 		for ( var j = 0; j < oRow.length; j++ ) {
-			var hexClasses = "hex " + resourceNumToString(boardobj.rows[i][j].type);
-			var rankClasses = "rank " + ((resourceNumToString(boardobj.rows[i][j].type) == "desert" || resourceNumToString(boardobj.rows[i][j].type) == "water") ? "no-show " : "");
-			/*var rankDotClasses = "dots " + ((boardobj.rows[i][j].rank == "6" || boardobj.rows[i][j].rank == "8") ? "five-dot" : "")
-						   + ((boardobj.rows[i][j].rank == "5" || boardobj.rows[i][j].rank == "9") ? "four-dot" : "")
-						   + ((boardobj.rows[i][j].rank == "4" || boardobj.rows[i][j].rank == "10") ? "three-dot" : "")
-						   + ((boardobj.rows[i][j].rank == "3" || boardobj.rows[i][j].rank == "11") ? "two-dot" : "")
-						   + ((boardobj.rows[i][j].rank == "2" || boardobj.rows[i][j].rank == "12") ? "one-dot" : "");*/
-			eRow.append('<div class="' + hexClasses+ '" data-x="' + i + '" data-y="' + j + '"><span class="' + rankClasses + '">' + boardobj.rows[i][j].rank + '</span></div>');
+		    var hexClasses = "hex " + resourceNumToString(boardobj.rows[i][j].type);
+		    var rankClasses = "rank " + ((resourceNumToString(boardobj.rows[i][j].type) == "desert" || resourceNumToString(boardobj.rows[i][j].type) == "water") ? "no-show " : "");
+		    /*var rankDotClasses = "dots " + ((boardobj.rows[i][j].rank == "6" || boardobj.rows[i][j].rank == "8") ? "five-dot" : "")
+		      + ((boardobj.rows[i][j].rank == "5" || boardobj.rows[i][j].rank == "9") ? "four-dot" : "")
+		      + ((boardobj.rows[i][j].rank == "4" || boardobj.rows[i][j].rank == "10") ? "three-dot" : "")
+		      + ((boardobj.rows[i][j].rank == "3" || boardobj.rows[i][j].rank == "11") ? "two-dot" : "")
+		      + ((boardobj.rows[i][j].rank == "2" || boardobj.rows[i][j].rank == "12") ? "one-dot" : "");*/
+		    eRow.append('<div class="' + hexClasses+ '" data-x="' + i + '" data-y="' + j + '"><span class="' + rankClasses + '">' + boardobj.rows[i][j].rank + '</span></div>');
+		    eRow.append('<span class="intersect right" data-a="' + i + '" data-b="' + j + '" data-c="1">');
+		    eRow.append('<span class="intersect bottom" data-a="' + i + '" data-b="' + j + '" data-c="0">');
 		}
-	}
-
-});
-
+	    }
+	    
+	});
+    
 });
