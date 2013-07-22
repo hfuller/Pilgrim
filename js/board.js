645004bec5a09513e666a9d452ba668b043d27c6
javakat <javakatdesign@gmail.com>
2013-07-22 11:30:23 -0500

    Add shortcuts to menus. Fix spin-loader layering on board.html.

--- js/board.js
+++ js/board.js
@@ -208,7 +208,7 @@ function registerMenuHandlers() {
     $('#actions,#their-stats').on('click', function () {
         // if the menu corresponding to the clicked button is already visible, then toggle every menu off.
         // (more specifically, if this button's first child with class 'top-menu' has an opacity of 1.0, then it is shown, and so toggle every menu off.
-        if($($(this).children('.top-menu')[0]).css('opacity') == 1.0) menuToggle();
+        if($($(this).children('.top-menu')[0]).css('opacity') > 1.0) menuToggle();
         // otherwise, toggle every menu off, and then toggle the menu corresponding to the clicked button on.
         // stopPropagation allows us to click out to exit a menu, but not cause every click on the page to exit a menu -- only ones that are not on a menu button.
         else {
@@ -226,8 +226,14 @@ function registerMenuHandlers() {
 		if($($('#their-stats').children('.top-menu')[0]).css('opacity') > 0) menuToggle();
 		else menuToggle(revealMenu, $('#their-stats'));
 		break;
+	  case 65: // a
+		if($($('#actions').children('.top-menu')[0]).css('opacity') > 0) menuToggle();
+		else menuToggle(revealMenu, $('#actions'));
+		break;
 	  case 27: // esc
        		menuToggle();
+		break;
+	  default:
 		break;
        }
     });
@@ -238,7 +244,7 @@ function registerMenuHandlers() {
 function menuToggle(callback, m){
 	var shown_top_menus = $('.top-menu').filter(function (index) {
 		
-		return $(this).css('opacity') == 1.0;
+		return $(this).css('opacity') > 1.0;
 	});
     	if(shown_top_menus.length > 0) {
 		shown_top_menus.animate(
