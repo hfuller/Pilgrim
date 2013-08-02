b81dc4bff9d18744a5997549ed38c6dc646bab81
Jake <javakatdesign@gmail.com>
2013-08-01 19:58:02 -0500

    Create lobby css and js. Update lobby.php.

--- lobby.php
+++ lobby.php
@@ -1,1 +1,18 @@
-There is no lobby. Are you kidding?
+<?php
+require_once("pilgrimlib.php");
+$page = new Page("Lobby");
+echo($page->getHeader($page->getTitle()));
+?>
+
+<body>
+    
+<div class="pure-g-r" style="background: #c48; height: 200px;">
+    <div class="pure-u-1-3" style="background: #8c4; height: 90px;">
+        <p style="color: #fff;">WHITE TEXT</p>
+    </div>
+    <div class="pure-u-1-2" style="background: #48c; height: 90px;">
+        <p style="color: #fff;">WHITE TEXT</p>
+    </div>
+</div>
+    
+</body>
